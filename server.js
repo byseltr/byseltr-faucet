9da7c48b6150149055f5013bbf89ac7724bfb899
AndrewSich <andrewsetyawan123@gmail.com>
2023-09-26 23:40:31 +0700

    update file server.js

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 const router = express.Router()
 
 // `api` router paths
-router.get('/', (req, res) => {
-	res.send('api')
-})
-
-router.get('/goerli', (req, res) => {
-	res.send('api -> goerli')
-})
-
+router.get('/', (req, res) => {res.send('api')})
+router.get('/test', (req, res) => see(res, "hallo test"))
+const see = (res, message) => {
+	res.send(message)
+}
 
 // main paths
 app.use('/api', router)
-
-app.get('/support', (req, res) => {
-	var mail = "support@byseltr.xyz"
-	res.send(`mail to: ${mail}`)
+app.get('/support', (req, res) => {res
+	.send('mail to: support@byseltr.xyz')
 })
 
+app.get('/status', (req, res) => {res.status(200)
+	.send('server is normal')
+})
+
+app.get('/ip', (req, res) => {res.status(200).send({
+	ip: req.headers["cf-connecting-ip"] || req.ip
+})})
+
+app.get('/', (req, res) => {res.send('developed by andrew setyawan')})
+app.get('*', (req, res) => {res.send('404: page not found!')})
 
 // running server
-app.listen(5000, () => {
-	console.log('server is running')
-})
+const port = process.env.PORT || 5000
+app.listen(port, () => {console.log(`server is running on port ${port}`)})
